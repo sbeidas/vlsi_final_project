fe284a979a6d89d12b6ab4c4b08416159dc7caed
eyoela <eyoela@gmail.com>
2013-04-24 23:27:15 -0500

    32 bit carry ripple

--- carry_skip_adder32.v
+++ carry_skip_adder32.v
@@ -70,15 +70,31 @@ endmodule //skip logic
 //carry-skip-adder
 module skip_adder8(s, co, a, b, ci);
    
-   output [7:0] s;
+   output [31:0] s;
    output 	co;
-   input [7:0] 	a, b;
+   input [31:0] 	a, b;
    input 	ci;
-   wire 	c1,c2;
+   wire 	c1,c2,c3,c4,c5,c6,c7,c8;
    adder4	a0(s[3:0],c1, a[3:0],b[3:0], ci);
    skiplogic s0(c2, a[3:0],b[3:0],ci,c1);
    adder4 	a1(s[7:4],c1, a[7:4],b[7:4], ci);
-   skiplogic s1(co,a[7:4],b[7:4],c2,c1);   
+   skiplogic s1(c3,a[7:4],b[7:4],c2,c1); 
+   
+   adder4 	a2(s[11:8],c1, a[11:8],b[11:8], ci);
+   skiplogic s2(co,a[11:8],b[11:8],c3,c1); 
+   adder4 	a3(s[15:12],c1, a[15:12],b[15:12], ci);
+   skiplogic s3(co,a[15:12],b[15:12],c2,c1);   
+   
+   adder4 	a4(s[19:16],c1, a[19:16],b[19:16], ci);
+   skiplogic s4(co,a[19:16],b[19:16],c2,c1); 
+   adder4 	a5(s[23:20],c1, a[23:20],b[23:20], ci);
+   skiplogic s5(co,a[23:20],b[23:20],c2,c1); 
+   
+   adder4 	a6(s[27:24],c1, a[27:24],b[27:24], ci);
+   skiplogic s6(co,a[27:24],b[27:24],c2,c1); 
+   adder4 	a7(s[31:28],c1, a[31:28],b[31:28], ci);
+   skiplogic s7(co,a[31:8],b[31:28],c2,c1); 
+   
    
 endmodule // carry-skip-adder8
 
