29cf4fdc149abcf8d3a361fd343e5123c2c1cd11
sbeidas <subhibeidas@gmail.com>
2013-04-25 01:41:09 -0700

    Added 4bit-mux

--- mux.v
+++ mux.v
@@ -11,3 +11,18 @@ module  mux(in_0,in_1 ,sel,mux_out);
 		end
 	end
 endmodule //mux
+
+module  mux_4bit(in_0,in_1 ,sel,mux_out);
+	input [3:0] in_0, in_1;
+	input sel ;
+	output [3:0]  mux_out;
+	reg [3:0]  mux_out;
+	always @ (sel or in_0 or in_1)
+	begin : MUX
+		if (sel == 1'b0) begin
+ 			mux_out = in_0;
+		end else begin
+			mux_out = in_1 ;
+		end
+	end
+endmodule //mux
