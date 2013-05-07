ba1681e233d85df83fbe1752336b4d542ed4891a
eyoela <eyoela@gmail.com>
2013-05-07 00:49:35 -0500

    Working sklansky 8 bit

--- sklansky_adder8.v
+++ sklansky_adder8.v
@@ -14,6 +14,7 @@ module pg_8block(p_out,g_out,a,b,cin);
   
   
   assign g_out[0] = cin;
+  assign p_out[0] = 0;
   pg_block pg1(g_out[1],p_out[1],a[0],b[0]);
   pg_block pg2(g_out[2],p_out[2],a[1],b[1]);
   pg_block pg3(g_out[3],p_out[3],a[2],b[2]);
@@ -38,6 +39,8 @@ module sum_8block(s,cout,p,g);
   output [7:0] s;
   output cout;
 
+  wire w1;
+
   sum_block s1(s[0],g[0],p[1]);
   sum_block s2(s[1],g[1],p[2]);
   sum_block s3(s[2],g[2],p[3]);
@@ -46,8 +49,8 @@ module sum_8block(s,cout,p,g);
   sum_block s6(s[5],g[5],p[6]);
   sum_block s7(s[6],g[6],p[7]);
   sum_block s8(s[7],g[7],p[8]);
-  				//calculate cout
-  and(w1,g[7],p[7]);
+                  //calculate cout
+  and(w1,g[7],p[8]);
   or(cout,w1,g[8]);
 
 endmodule //sum_8block
@@ -63,7 +66,7 @@ module grey_box(g_out, p ,g,g_old);
 endmodule // grey_box
 
 
-module black_box(g_out,p_out,p,g,p_old,g_old);
+module black_box(p_out,g_out,p,g,p_old,g_old);
   input p,g,p_old,g_old;
   output g_out,p_out;
 
@@ -81,35 +84,35 @@ module sklansky_logic8(p_out,g_out,p,g);
 
   wire  w1p,w1g,w2p,w2g,w3p,w3g,w4p,w4g,w5p,w5g, gout1,gout3;
 
-  assign    g_out[0] = g[0];			//pin 0
+  assign    g_out[0] = g[0];            //pin 0
   assign    p_out[0] = p[0];
 
-  grey_box  gb1(gout1,p[1],g[1],g[0]);	//pin 1
+  grey_box  gb1(gout1,p[1],g[1],g[0]);    //pin 1
   assign    p_out[1] = p[1];
   assign    g_out[1] = gout1;
 
-  grey_box  gb2(g_out[2],p[2],g[2],g[1]);	//pin 2
+  grey_box  gb2(g_out[2],p[2],g[2],gout1);    //pin 2
   assign    p_out[2] = p[2];
 
-  black_box bb1(w1p,w1g,p[3],g[3],p[2],g[2]);	//pin 3
+  black_box bb1(w1p,w1g,p[3],g[3],p[2],g[2]);    //pin 3
   grey_box  gb3(gout3,w1p,w1g,gout1);
   assign    p_out[3] = p[3];
   assign    g_out[3] = gout3;
 
-  grey_box  gb4(g_out[4],p[4],g[4],gout3);	//pin 4
+  grey_box  gb4(g_out[4],p[4],g[4],gout3);    //pin 4
   assign    p_out[4] = p[4];
-  black_box bb2(w2p,w2g,p[5],g[5],p[4],g[4]);	//pin 5
+  black_box bb2(w2p,w2g,p[5],g[5],p[4],g[4]);    //pin 5
   grey_box  gb5(g_out[5],w2p,w2g,gout3);
   assign    p_out[5] = p[5];
-  black_box bb3(w3p,w3g,p[6],g[6],w2p,w2g);	//pin 6
+  black_box bb3(w3p,w3g,p[6],g[6],w2p,w2g);    //pin 6
   grey_box  gb6(g_out[6],w3p,w3g,gout3);
   assign    p_out[6] = p[6];
-  black_box bb4(w4p,w4g,p[7],g[7],p[6],g[6]);	//pin 7
+  black_box bb4(w4p,w4g,p[7],g[7],p[6],g[6]);    //pin 7
   black_box bb5(w5p,w5g,w4p,w4g,w2p,w2g);
   grey_box  gb7(g_out[7],w5p,w5g,gout3);
   assign    p_out[7] = p[7];
   
- // grey_box  gb8(g_out[8],p[8],g[8],g[7]);	//pin 8
+  assign    g_out[8]=g[8];            //pin 8
   assign    p_out[8]=p[8];
 
 endmodule //slansky_logic_8bit
@@ -127,6 +130,3 @@ module sklansky_adder8(s,cout,a,b,cin);
   sum_8block sb8(s,cout,p_out,g_out);
 
 endmodule //sklansky_adder8
-  
-  
-  
